918880df11143cb2e5fc9f8181b66d95c8d13ff4
firstdemo <1057314353@qq.com>
2020-03-28 16:10:35 +0800

    update Improving Deep Neural Networks

--- Improving Deep Neural Networks/Initialization/Initialization.py
+++ Improving Deep Neural Networks/Initialization/Initialization.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sklearn
 import sklearn.datasets
-from init_utils import sigmoid,relu,compute_loss,forward_propagation,backward_propagation
-from init_utils import update_parameters,predict,load_dataset,plot_decision_boundary,predict_dec
+from init_utils import sigmoid, relu, compute_loss, forward_propagation, backward_propagation
+from init_utils import update_parameters, predict, load_dataset, plot_decision_boundary, predict_dec
 
 
 # GRADED FUNCTION: initialize_parameters_zeros
@@ -22,12 +22,12 @@ def initialize_parameters_zeros(layers_dims):
                     bL -- bias vector of shape (layers_dims[L], 1)
     """
     parameters = dict()
-    L = len(layers_dims) # number of layers in the network
+    L = len(layers_dims)  # number of layers in the network
 
-    for l in range(1,L):
+    for l in range(1, L):
         ### START CODE HERE ### (≈ 2 lines of code)
-        parameters['W' + str(l)] = np.zeros((layers_dims[l],layers_dims[l-1]))
-        parameters['b' + str(l)] = np.zeros((layers_dims[l],1))
+        parameters['W' + str(l)] = np.zeros((layers_dims[l], layers_dims[l - 1]))
+        parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
         ### END CODE HERE ###
 
     return parameters
@@ -84,14 +84,15 @@ def initialize_parameters_he(layers_dims):
 
     for l in range(1, L + 1):
         ### START CODE HERE ### (≈ 2 lines of code)
-        parameters['W' + str(l)] = np.random.randn(layers_dims[l],layers_dims[l-1])*np.sqrt(2 / layers_dims[l - 1])
-        parameters['b' + str(l)] = np.zeros((layers_dims[l],1))
+        parameters['W' + str(l)] = np.random.randn(layers_dims[l], layers_dims[l - 1]) * np.sqrt(2 / layers_dims[l - 1])
+        parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
         ### END CODE HERE ###
 
     return parameters
 
+
 # Neural Network model
-def model(X,Y,learning_rate=0.01,num_iterations=15000,print_cost=True,initialization="he"):
+def model(X, Y, learning_rate=0.01, num_iterations=15000, print_cost=True, initialization="he"):
     """
     Implements a three-layer neural network: LINEAR->RELU->LINEAR->RELU->LINEAR->SIGMOID.
 
@@ -108,34 +109,34 @@ def model(X,Y,learning_rate=0.01,num_iterations=15000,print_cost=True,initializa
     """
 
     grads = dict()
-    costs = list() # to keep track of the loss
-    m = X.shape[1] # number of examples
-    layers_dims = [X.shape[0],10,5,1]
+    costs = list()  # to keep track of the loss
+    m = X.shape[1]  # number of examples
+    layers_dims = [X.shape[0], 10, 5, 1]
 
     # Initialize parameters dictionary.
     if initialization == "zeros":
-         parameters = initialize_parameters_zeros(layers_dims)
+        parameters = initialize_parameters_zeros(layers_dims)
     elif initialization == "random":
         parameters = initialize_parameters_random(layers_dims)
     elif initialization == "he":
         parameters = initialize_parameters_he(layers_dims)
     # Loop (gradient descent)
-    for i in range(0,num_iterations):
+    for i in range(0, num_iterations):
         # Forward propagation: LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SIGMOID.
-        a3,cache = forward_propagation(X,parameters)
+        a3, cache = forward_propagation(X, parameters)
 
         # Loss
-        cost = compute_loss(a3,Y)
+        cost = compute_loss(a3, Y)
 
         # Backward propagation.
-        grads = backward_propagation(X,Y,cache)
+        grads = backward_propagation(X, Y, cache)
 
         # Update parameters.
-        parameters = update_parameters(parameters,grads,learning_rate)
+        parameters = update_parameters(parameters, grads, learning_rate)
 
         # Print the loss every 1000 iterations
         if print_cost and i % 1000 == 0:
-            print("Cost after iteration {}: {}".format(i,cost))
+            print("Cost after iteration {}: {}".format(i, cost))
             costs.append(cost)
     # plot the loss
     plt.plot(costs)
@@ -146,21 +147,21 @@ def model(X,Y,learning_rate=0.01,num_iterations=15000,print_cost=True,initializa
 
     return parameters
 
-if __name__ == '__main__':
 
-    plt.rcParams['figure.figsize'] = (7.0,4.0) # set default size of plots
+if __name__ == '__main__':
+    plt.rcParams['figure.figsize'] = (7.0, 4.0)  # set default size of plots
     plt.rcParams['image.interpolation'] = 'nearest'
     plt.rcParams['image.cmap'] = 'gray'
 
     # load image dataset:blue/red dots in circles
-    train_X,train_Y,test_X,test_Y = load_dataset()
+    train_X, train_Y, test_X, test_Y = load_dataset()
 
     # initialize_parameters_zeros
-    parameters=model(train_X,train_Y,initialization="zeros")
+    parameters = model(train_X, train_Y, initialization="zeros")
     print("On the train set:")
-    predictions_train = predict(train_X,train_Y,parameters)
+    predictions_train = predict(train_X, train_Y, parameters)
     print("On the test set:")
-    predictions_test = predict(test_X,test_Y,parameters)
+    predictions_test = predict(test_X, test_Y, parameters)
     print(predictions_train)
     print(predictions_test)
 
@@ -181,4 +182,3 @@ if __name__ == '__main__':
     predictions_test = predict(test_X, test_Y, parameters)
     print(predictions_train)
     print(predictions_test)
-
